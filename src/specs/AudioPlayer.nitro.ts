ffe3dc070f4bdedd292551e4e3a6db9bad38a510
longkp <longkp@lasbom.com>
2026-04-13 11:15:40 +0700

    fix

--- src/specs/AudioPlayer.nitro.ts
+++ src/specs/AudioPlayer.nitro.ts
@@ -1,32 +1,16 @@
 import { type HybridObject } from 'react-native-nitro-modules'
-
-export interface Track {
-  id: string
-  trackId: string
-  title: string
-  artist: string
-  trackThumbnailUrl: string
-  trackUrl: string
-  type: number // 0: Online, 1: Offline
-}
-
 export interface AudioPlayer extends HybridObject<{
   ios: 'swift'
   android: 'kotlin'
 }> {
-  setPlaylist(tracks: Track[], index: number): void
-  next(): void
-  previous(): void
-
+  load(url: string): void
   play(): void
   pause(): void
   stop(): void
-  seek(seconds: number): void
-
   readonly isPlaying: boolean
+  volume: number
   readonly duration: number
   readonly currentTime: number
-  readonly currentTrackId: string
-
-  updateTrackUrl(index: number, url: string): void
+  seek(seconds: number): void
+  skip(seconds: number): void
 }
